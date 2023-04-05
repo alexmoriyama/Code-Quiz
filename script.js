703392b2ec362ca0f23ee3c3986ed6cfa8c88a57
Alex Moriyama <alex.moriyama@gmail.com>
2023-04-04 17:25:17 -0700

    final changes

--- script.js
+++ script.js
@@ -10,9 +10,14 @@ var quizQuestions = [
         answer: "Magic Johnson",
     },
     {
-        question:"Who played for the Lakers?",
-        choices: ["Larry Bird","Magic Johnson","Bill Russell","John Stockton"],
-        answer: "Magic Johnson",
+        question:"Who has the most rings?",
+        choices: ["Charles Barkley","Steve Nash","Dirk Nowitzki","Kobe Bryant"],
+        answer: "Kobe Bryant",
+    },    
+    {
+        question:"Who leads the NBA in most points scored?",
+        choices: ["Kareem Abdul-Jabar","Kobe Bryant","Lebron James","Shaq"],
+        answer: "Lebron James",
     }
     // This is the format for the questions repeat for all.
 ]
@@ -34,32 +39,25 @@ function timerStart(){
     timerDiv.textContent = timerClock
     if(timerClock===0){
         clearInterval(timer)
-        // invoke lose game function
+        window.alert("Game Over!")
     }
     },1000)
 }
 
 function showQuestions(){
+
     var questionGrab = quizQuestions[questionGrabIndex]
     var questionTitle = document.querySelector(".question-container")
     var optionsContainer = document.querySelector(".option-container")
     var optionButton = document.querySelector(".option")
     questionTitle.textContent = questionGrab.question
-
-    // Append choices
-    // var choices = document.querySelector(".option")
-    // for (var i = 0;i<quizQuestions[i].choices.length;i++){
-    //     choices.textContent=quizQuestions[i].choices
-    //     optionsContainer.append(choices)
-    // }
+    optionsContainer.innerHTML = ""
     questionGrab.choices.forEach(function(choiceButton,i){
        var choiceIndex = document.createElement("button")
        choiceIndex.setAttribute('class', "option") 
        choiceIndex.setAttribute("value", choiceButton)
        choiceIndex.textContent = i + 1 + ". " + choiceButton
-
-    //    choiceIndex.onclick = checkAnswer;
-
+       choiceIndex.addEventListener('click', checkAnswer)
        optionsContainer.appendChild(choiceIndex)
     })
 }
@@ -68,9 +66,10 @@ function checkAnswer(event){
     if(event.target.id===quizQuestions[questionGrabIndex].answer){
         window.alert("Correct!")
     }else{
-        time-=10
-        showQuestions()
+        timerClock-=10
     }
+    questionGrabIndex++
+    showQuestions()
 }
 
-// Get the checkAnswer function working properly by displaying the next question, use local storage to enable the initials and score
+// use local storage to enable the initials and score, end the game at the end
